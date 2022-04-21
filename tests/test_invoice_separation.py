eeb8e835fd3215e93bc6b62fea6d2bf73a61806d
iliayar <iliayar3@gmail.com>
2022-04-21 10:10:09 +0300

    feat server: improve test

--- tests/test_invoice_separation.py
+++ tests/test_invoice_separation.py
@@ -13,9 +13,11 @@ api = DefaultApi(api_client=api_client)
 
 login1 = f'user-{uuid4()}'
 login2 = f'user-{uuid4()}'
+login3 = f'user-{uuid4()}'
 
 creds1 = Credentials(login=login1, password='test')
 creds2 = Credentials(login=login2, password='test')
+creds3 = Credentials(login=login3, password='test')
 
 r = api.user_register_post(body=creds1)
 token1 = api.user_login_post(body=creds1)
@@ -23,7 +25,10 @@ token1 = api.user_login_post(body=creds1)
 r = api.user_register_post(body=creds2)
 token2 = api.user_login_post(body=creds2)
 
-req = InvoiceSeparationRequest(invoice=100, users=[login2])
+r = api.user_register_post(body=creds3)
+token3 = api.user_login_post(body=creds3)
+
+req = InvoiceSeparationRequest(invoice=100, users=[login2, login3])
 
 api_client1 = ApiClient(configuration=configuration)
 api_client1.set_default_header('X-Api-Key', token1)
@@ -33,11 +38,14 @@ api_client2 = ApiClient(configuration=configuration)
 api_client2.set_default_header('X-Api-Key', token2)
 api2 = DefaultApi(api_client=api_client2)
 
+api_client3 = ApiClient(configuration=configuration)
+api_client3.set_default_header('X-Api-Key', token3)
+api3 = DefaultApi(api_client=api_client3)
+
 api1.invoice_separation_post(body=req)
 
-
 debt = api2.debt_get(login1);
-assert debt == 100
+assert debt == 50
 
 req = UsernameRequest(username=login1)
 api2.debt_post(body=req)
@@ -45,5 +53,5 @@ api2.debt_post(body=req)
 debt = api2.debt_get(login1);
 assert debt == 0
 
-print(login1)
-print(token1)
+debt = api3.debt_get(login1);
+assert debt == 50
