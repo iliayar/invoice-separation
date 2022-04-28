a572121425fb1e805cc4657c24564208c4b888d2
dmitry-fadeev <dmitry-fadeev@yandex-team.ru>
2022-04-28 14:31:03 +0300

    Update API for mobile

--- mobile/app/src/main/java/com/example/invoicesep/api/JsonPlaceHolderAPI.kt
+++ mobile/app/src/main/java/com/example/invoicesep/api/JsonPlaceHolderAPI.kt
@@ -9,29 +9,29 @@ import retrofit2.http.*
 
 interface JsonPlaceHolderApi {
     @POST("user/register")
-    suspend fun registerUser(@Header("X-Api-Key") apiKey: String,@Body userLogin: UserLogin): Response<Unit>
+    suspend fun registerUser(@Body userLogin: UserLogin): Response<Unit>
 
     @POST("user/login")
-    suspend fun loginUser(@Header("X-Api-Key") apiKey: String,@Body userLogin: UserLogin): Response<Unit>
+    suspend fun loginUser(@Body userLogin: UserLogin): Response<String>
 
     @POST("contacts")
     suspend fun postContacts(
-        @Header("X-Api-Key") apiKey: String,
-        @Body users: List<String>
+            @Header("X-Api-Key") apiKey: String,
+            @Body users: List<String>
     ): Response<Unit>
 
     @GET("contacts")
-    suspend fun getContacts(@Header("X-Api-Key") apiKey: String,): Response<List<String>>
+    suspend fun getContacts(@Header("X-Api-Key") apiKey: String): Response<List<String>>
 
     @POST("invoice-separation")
-    suspend fun invoiceSeparation(@Header("X-Api-Key") apiKey: String,@Body invoiceSeparation: InvoiceSeparation): Response<Unit>
+    suspend fun invoiceSeparation(@Header("X-Api-Key") apiKey: String, @Body invoiceSeparation: InvoiceSeparation): Response<Unit>
 
     @POST("debt")
     suspend fun postDebt(@Header("X-Api-Key") apiKey: String, @Body user: User): Response<Unit>
 
     @GET("debt")
     suspend fun getDebt(
-        @Header("X-Api-Key") apiKey: String,
-        @Query("user") user: String
+            @Header("X-Api-Key") apiKey: String,
+            @Query("user") user: String
     ): Response<Int>
 }
